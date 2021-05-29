dc9d99730799290de3daf50e03cde4f0004940d1
smanivesh <70524700+smanivesh@users.noreply.github.com>
2021-05-29 10:47:09 +0530

    Add files via upload

---
+++ androiddrawing/MainActivity.java
@@ -0,0 +1,74 @@
+package com.sm.androiddrawing;
+
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    DrawingView drawingView;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawingView = findViewById(R.id.canvas);
+        drawingView.setOnTouchListener(new TouchListener());
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                drawingView.setCurrentWidth(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void setColor(View view) {
+        ColorDrawable buttonColor = (ColorDrawable) view.getBackground();
+        drawingView.setCurrentColor(buttonColor.getColor());
+        if (view.getTag() != null && view.getTag().equals("eraser")) {
+            drawingView.setCurrentWidth(seekBar.getProgress() * 4);
+        } else {
+            drawingView.setCurrentWidth(seekBar.getProgress());
+        }
+    }
+
+    public void deleteDrawing(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to erase everything?")
+                .setTitle("Delete Drawing")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        drawingView.erase();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
